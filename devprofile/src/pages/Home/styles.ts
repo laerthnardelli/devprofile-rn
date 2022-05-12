49e9ba8eb0bc0eb7d47edb3ac332b9d70b495904
laerthnardelli <laerthnardelli@gmail.com>
2022-05-12 20:26:40 -0300

    added global theme color in styles HomePage

--- devprofile/src/pages/Home/styles.ts
+++ devprofile/src/pages/Home/styles.ts
@@ -4,12 +4,12 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #121214;
+  background-color: ${({ theme }) => theme.colors.dark};
 `;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 24px;
   font-weight: bold;
-  color: #f1f1f1;
+  color: ${({ theme }) => theme.colors.light};
 `;
