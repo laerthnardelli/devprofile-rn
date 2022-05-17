5e1971da976b90901eea1a91ad100914560097b5
laerthnardelli <laerthnardelli@gmail.com>
2022-05-16 21:12:30 -0300

    added Button component - part 02

--- devprofile/src/pages/Signin/index.tsx
+++ devprofile/src/pages/Signin/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
+import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 import { Container, Content, Title } from './styles';
 
@@ -11,9 +12,10 @@ export const SignIn: React.FunctionComponent = () => {
     >
       <Container>
         <Content>
-          <Title>Faça seu logon</Title>
+          <Title>Faça seu login</Title>
           <Input placeholder="Email" />
           <Input placeholder="Senha" />
+          <Button title="Entrar" />
         </Content>
       </Container>
     </ScrollView>
