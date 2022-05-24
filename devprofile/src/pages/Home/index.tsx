bb42a417bcced4182b7cdc9319d3e8b9bdf99fff
laerthnardelli <laerthnardelli@gmail.com>
2022-05-23 21:41:11 -0300

    Creating the User Profile Page - Part 2

--- devprofile/src/pages/Home/index.tsx
+++ devprofile/src/pages/Home/index.tsx
@@ -61,12 +61,16 @@ export const Home: React.FunctionComponent = () => {
     navigate('UserDetails', { userId });
   };
 
+  const handleUserProfile = () => {
+    navigate('UserProfile');
+  };
+
   return (
     <Container>
       <Header>
         <UserWrapper>
           <UserInfo>
-            <UserAvatarButton onPress={() => {}}>
+            <UserAvatarButton onPress={handleUserProfile}>
               <UserAvatar
                 source={
                   user.avatar_url ? { uri: user.avatar_url } : avatarDefault
