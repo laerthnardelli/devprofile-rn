8a2ff4909cc0d117ab0660c8a44da62a51cdc0fe
laerthnardelli <laerthnardelli@gmail.com>
2022-05-23 21:02:37 -0300

    Creating the page to show a UserDetails - part 4

--- devprofile/src/pages/UserDetails/index.tsx
+++ devprofile/src/pages/UserDetails/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
 
+import avatarDefault from '../../assets/avatar02.png';
+import { useAuth } from '../../context/AuthContext';
 import { IUser } from '../../model/user';
 import { api } from '../../services/api';
 
@@ -30,6 +32,7 @@ export const UserDetails: React.FunctionComponent = () => {
   const [userDetails, setUserDetails] = React.useState<IUser>({} as IUser);
   const route = useRoute();
   const { userId } = route.params as RouteParams;
+  const { user } = useAuth();
 
   React.useEffect(() => {
     const loadUser = async () => {
@@ -48,12 +51,19 @@ export const UserDetails: React.FunctionComponent = () => {
           <Icon />
         </GoBackButton>
         <HeaderTile>Usuários</HeaderTile>
-        <UserAvatar />
+        <UserAvatar
+          source={user.avatar_url ? { uri: user.avatar_url } : avatarDefault}
+        />
       </Header>
       <Content>
         <ContentTitle>Detalhes do Usuário</ContentTitle>
-        <UserDetailAvatar />
-
+        <UserDetailAvatar
+          source={
+            userDetails.avatar_url
+              ? { uri: userDetails.avatar_url }
+              : avatarDefault
+          }
+        />
         <UserNameDetail>
           <NameTitle>NAME</NameTitle>
           <NameData>{userDetails.name}</NameData>
